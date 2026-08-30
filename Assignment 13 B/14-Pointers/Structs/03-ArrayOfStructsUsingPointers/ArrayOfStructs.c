981d77c7067e7c5a830c8fe7b93b86b7a90e6db5
Shital Shirole <shitalshirole4044@gmail.com>
2026-08-30 23:29:05 +0530

    OUTPUT

--- Assignment 13 B/14-Pointers/Structs/03-ArrayOfStructsUsingPointers/ArrayOfStructs.c
+++ Assignment 13 B/14-Pointers/Structs/03-ArrayOfStructsUsingPointers/ArrayOfStructs.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <ctype.h>
-
 #define NAME_LENGTH 100
 #define MARITAL_STATUS 10
-
 struct Employee
 {
     char name[NAME_LENGTH];
@@ -12,18 +11,15 @@ struct Employee
     float salary;
     char marital_status;
 };
-
 int main(void)
 {
     void MyGetString(char[], int);
-
     struct Employee *pEmployeeRecord = NULL;
     int num_employees, i;
-
     printf("\n\n");
     printf("Enter Number Of Employees Whose Details You Want To Record : ");
     scanf("%d", &num_employees);
-
+    while (getchar() != '\n'); // flush newline left by scanf("%d")
     printf("\n\n");
     pEmployeeRecord = (struct Employee *)malloc(sizeof(struct Employee) * num_employees);
     if (pEmployeeRecord == NULL)
@@ -33,37 +29,32 @@ int main(void)
     }
     else
         printf("SUCCESSFULLY ALLOCATED MEMORY FOR %d EMPLOYEES !!!\n\n", num_employees);
-
     for (i = 0; i < num_employees; i++)
     {
         printf("\n\n\n\n");
         printf("********** DATA ENTRY FOR EMPLOYEE NUMBER %d **********\n", (i + 1));
-
         printf("\n\n");
         printf("Enter Employee Name : ");
         MyGetString(pEmployeeRecord[i].name, NAME_LENGTH);
-
         printf("\n\n\n");
         printf("Enter Employee's Age (in years) : ");
         scanf("%d", &pEmployeeRecord[i].age);
-
+        while (getchar() != '\n'); // flush newline left by scanf("%d")
         printf("\n\n");
         printf("Enter Employee's Sex (M/m For Male, F/f For Female) : ");
-        pEmployeeRecord[i].sex = getch();
-        printf("%c", pEmployeeRecord[i].sex);
+        pEmployeeRecord[i].sex = getchar();
+        while (getchar() != '\n'); // consume the Enter keystroke
         pEmployeeRecord[i].sex = toupper(pEmployeeRecord[i].sex);
-
         printf("\n\n\n");
         printf("Enter Employee's Salary (in Indian Rupees) : ");
         scanf("%f", &pEmployeeRecord[i].salary);
-
+        while (getchar() != '\n'); // flush newline left by scanf("%f")
         printf("\n\n");
         printf("Is The Employee Married? (Y/y For Yes, N/n For No) : ");
-        pEmployeeRecord[i].marital_status = getch();
-        printf("%c", pEmployeeRecord[i].marital_status);
+        pEmployeeRecord[i].marital_status = getchar();
+        while (getchar() != '\n'); // consume the Enter keystroke, ready for next loop's MyGetString
         pEmployeeRecord[i].marital_status = toupper(pEmployeeRecord[i].marital_status);
     }
-
     printf("\n\n\n\n");
     printf("********** DISPLAYING EMPLOYEE RECORDS **********\n\n");
     for (i = 0; i < num_employees; i++)
@@ -71,52 +62,42 @@ int main(void)
         printf("************ EMPLOYEE NUMBER %d **********\n\n", (i + 1));
         printf("Name           : %s\n", pEmployeeRecord[i].name);
         printf("Age            : %d years\n", pEmployeeRecord[i].age);
-
         if (pEmployeeRecord[i].sex == 'M')
             printf("Sex            : Male\n");
         else if(pEmployeeRecord[i].sex == 'F')
             printf("Sex            : Female\n");
         else
             printf("Sex            : Invalid Data Entered\n");
-
         printf("Salary         : Rs. %f\n", pEmployeeRecord[i].salary);
-
         if (pEmployeeRecord[i].marital_status == 'Y')
             printf("Marital Status : Married\n");
         else if (pEmployeeRecord[i].marital_status == 'N')
             printf("Marital Status : Unmarried\n");
         else
             printf("Marital Status : Invalid Data Entered\n");
-
         printf("\n\n");
     }
-
     if (pEmployeeRecord)
     {
         free(pEmployeeRecord);
         pEmployeeRecord = NULL;
         printf("MEMORY ALLOCATED TO %d EMPLOYEES HAS BEEN SUCCESSFULLY FREED !!! \n\n", num_employees);
     }
-
     return(0);
 }
-
 void MyGetString(char str[], int str_size)
 {
     int i;
     char ch = '\0';
-
     i = 0;
     do
     {
-        ch = getch();
+        ch = getchar();
         str[i] = ch;
-        printf("%c", str[i]);
         i++;
-    }while ((ch != '\r') && (i < str_size));
-
+    } while ((ch != '\n') && (i < str_size)); // '\n' on macOS/Unix, NOT '\r' — see explanation above
     if (i == str_size)
         str[i - 1] = '\0';
     else
-        str[i] = '\0';
+        str[i - 1] = '\0'; // overwrite the captured '\n' with the terminator, not str[i]
 }
--- Assignment 14/17-GotoStatement/GotoStatement.c
+++ Assignment 14/17-GotoStatement/GotoStatement.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
 #include <string.h>
-
+#include <ctype.h>
 int main(void)
 {
     char ch, ch_i;
     unsigned int ascii_ch = 0;
-
     printf("\n\n");
     printf("Enter The First Character Of First Name : ");
-    ch = getch();
-
+    ch = getchar();
     ch = toupper(ch);
-
     for (ch_i = 'A'; ch_i <= 'Z'; ch_i++)
     {
         if (ch == ch_i)
@@ -20,23 +17,18 @@ int main(void)
             goto result_output;
         }
     }
-
     printf("\n\n");
     printf("Goto statement not executed, so printing \"Hello, world !!!\".\n");
-
     result_output:
     printf("\n\n");
-
     if (ascii_ch == 0)
     {
         printf("You must have a strange name! Could not find the character '%c' in the entire English Alphabet!\n", ch);
     }
-
     else
     {
         printf("Character '%c' found. It has ASCII value %u.\n", ch, ascii_ch);
     }
-
     printf("\n\n");
     return(0);
 }
